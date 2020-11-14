98cd27e7e26990e0c04c4639ea345b295366e481
Abe Ahn <abemsq@gmail.com>
2020-11-14 14:38:03 +0200

    Done

---
+++ 2-dog-years.js
@@ -0,0 +1,15 @@
+let myAge = 28;
+
+var earlyYears = 2;
+earlyYears *= 10.5;
+
+let laterYears = myAge - 2;
+laterYears *= 4;
+
+console.log(earlyYears);
+console.log(laterYears);
+
+let myAgeInDogYears = earlyYears + laterYears;
+console.log(myAgeInDogYears);
+
+let myName = 'Abe Ahn'.toLowerCase();
